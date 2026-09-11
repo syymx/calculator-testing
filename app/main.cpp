782c7684ce5b34e3e168d49420b65becf9f92e98
zym <2635955624@qq.com>
2026-09-11 09:00:15 +0800

    chore: restructure project for testing

---
+++ app/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "../src/calculator.hpp"
+
+
+int main()
+{
+    try
+    {
+        std::cout << calculator::eval("10 / 0") << std::endl;
+    }
+    catch (const calculator::error& e)
+    {
+        std::cerr << "Calculator Error: "
+                  << e.what()
+                  << std::endl;
+    }
+
+    return 0;
+}
